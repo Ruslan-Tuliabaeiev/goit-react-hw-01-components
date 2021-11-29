a1cc93930150893f718664b26fe867369bf685ea
руслан <82108672+Ruslan-Tuliabaeiev@users.noreply.github.com>
2021-11-29 21:45:52 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import PropTypes from "prop-types";
+//  import {BlocUser} from './components/Blocuser';
+// import './App.css';
+
+
+ import { FriendList } from "./components/FriendList";
+ 
+
+
+
+function App() {
+  return (
+<div>
+<FriendList></FriendList>
+ </div>
+  );
+}
+
+export default App;
